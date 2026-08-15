f1bafc9decd1b5f5b9df1e65fd12685e8063d481
Javohir <userjon800@gmail.com>
2026-08-15 14:09:35 +0500

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Linkedin",
+  description: "Linkedin clone app created by Javohir Xamdamboyev",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en" className={`${roboto.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
