f73f37fd2aab26a1034282f6cce04e5fe89bf167
Javohir <userjon800@gmail.com>
2026-08-18 14:59:47 +0500

    added library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Roboto, Figtree } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -14,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${roboto.variable} h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", roboto.variable, "font-sans", figtree.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,19 @@
+import { UploadButton } from "@/utils/uploadthing";
+export default function Navbar() {
+  return (
+    <div>
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </div>
+  );
+}
